7403d9fa887e5662ddc872c7a613f5d7e9967227
yulejessica <84048140+yulejessica@users.noreply.github.com>
2021-06-04 09:51:50 -0700

    Update script.js

--- Module4Solution/script.js
+++ Module4Solution/script.js
@@ -20,26 +20,40 @@ Hello Larry
 Hello Paula
 Hello Laura
 Good Bye Jim
+
 WARNING!!! WARNING!!!
 The code does NOT currently work! It is YOUR job to make it work
 as described in the requirements and the steps in order to complete this
 assignment.
 WARNING!!! WARNING!!!
+
 */
 
 // STEP 1:
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-
+(function () {
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+// Immediately Invoked Function Expression
+// IIFE
+  // helloSpeaker.SpeakHello();
+  // byeSpeaker.SpeakGoodBye();
+
+
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+ // for (/* fill in parts of the 'for' loop to loop over names array */) {
 
+
+// Short hand array creation
+
+for (var i = 0; i < names.length; i++) {
+  
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
@@ -47,16 +61,18 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
+  var firstLetter = names[i].charAt(0).toLowerCase();
 
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
   } else {
-    // helloSpeaker.xxxx
+    helloSpeaker.speak(names[i]);
   }
 }
+
+})();
