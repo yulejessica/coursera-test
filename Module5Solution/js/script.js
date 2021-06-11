65ceb1588e0fa888b5dcac8bd0c5e1af2bd225d4
yulejessica <yulejessica@Gmail.com>
2021-06-10 19:06:45 -0700

    update script and revert change in home snippet

--- Module5Solution/js/script.js
+++ Module5Solution/js/script.js
@@ -119,7 +119,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,
-        "short_name", chosenCategoryShortName);
+        "randomCategoryShortName", chosenCategoryShortName);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
